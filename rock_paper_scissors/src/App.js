3ae575bcc6fece19e3172b834a6b92dfdf7ad78a
COMBA <florent.comba@atos.net>
2019-06-18 17:09:30 +0200

    Ajout Tableau score et composant choix du mouvement

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 import './App.css';
 import PVC from './components/PlayerVsComputer'
 
-const initialState = {
-  wins: 0,
-  losses: 0,
-  draws: 0,
-  result: null
-}
-
-
 function App() {
   return (
     <div className="App">
