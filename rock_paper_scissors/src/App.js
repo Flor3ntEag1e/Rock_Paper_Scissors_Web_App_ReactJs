0bcab3281880ea24fb4b0123a678699486f952db
COMBA <florent.comba@atos.net>
2019-06-18 16:24:43 +0200

    Création du premier composant Joueur vs Ordinateur

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PVC from './components/PlayerVsComputer'
+
+const initialState = {
+  wins: 0,
+  losses: 0,
+  draws: 0,
+  result: null
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Rock Paper Scissors</h1>
+     <PVC/>
     </div>
   );
 }
---
+++ rock_paper_scissors/src/components/PlayerVsComputer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlayerVsComputer.css'
+
+const PVC = () => {
+    return (
+        <div className='PlayerComputerCanvas'>
+            <h2> Player vs Computer </h2>
+        </div>
+    )
+}
+
+export default PVC
